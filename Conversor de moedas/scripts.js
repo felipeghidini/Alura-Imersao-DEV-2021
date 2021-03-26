640c381a832900ce9645d566208cb2ea0125804b
lipeghidini18@gmail.com <lipeghidini18@gmail.com>
2021-03-26 09:11:40 -0300

    Aula 1 - Conversor de moedas

---
+++ Conversor de moedas/scripts.js
@@ -0,0 +1,5 @@
+var dolar = prompt('Qual o valor em dolar que deseja converter?')
+var dolar_value = parseFloat(dolar)
+var real = dolar_value*5.54
+var valor_real = real.toFixed(2)
+alert(valor_real)
